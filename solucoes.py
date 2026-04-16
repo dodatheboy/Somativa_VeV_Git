b37d1b4b34524d14b86512601392547e7d9df50e
rafaazk <rafaelbr396@gmail.com>
2026-04-16 10:31:43 -0300

    implementei a funcaon anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,11 @@
 def sao_anagrama(string1, string2):
-    # todo : implementar a logica 
-    pass
+
+    string1 = string1.replace(" ", "").lower()
+    string2 = string2.replace(" ", "").lower()
+    if sorted(string1) == sorted(string2):
+        return True
+    else:
+        return False
 
 def cifra_de_cesar(texto, deslocamento):
     # todo : implementar a logica
