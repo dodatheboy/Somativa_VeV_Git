a75b21abf4e4ddfab3cd6cdd8ac816d9ec2eac1c
rafaazk <rafaelbr396@gmail.com>
2026-04-16 10:43:52 -0300

    Implementei a logica da maior palavra

--- solucoes.py
+++ solucoes.py
@@ -11,6 +11,10 @@ def cifra_de_cesar(texto, deslocamento):
     # todo : implementar a logica
     pass
 
-def valida_cpf(cpf_string):
-    # todo : implementar a logica
-    pass
+def encontrar_maior_palavra(frase):
+    frase = [frase.split()]
+    maior_palavra = ""
+    for palavra in frase:
+        if len(palavra) > len(maior_palavra):
+            maior_palavra = palavra
+    return maior_palavra
