16584ea4f9250a97ef49794769c0ac904900c29b
Mateus Vieira Domingues <domingues.mateus@pucpr.edu.br>
2026-04-16 10:20:00 -0300

    commit inicial

---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagrama(string1, string2):
+    # todo : implementar a logica 
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    # todo : implementar a logica
+    pass
+
+def valida_cpf(cpf_string):
+    # todo : implementar a logica
+    pass
